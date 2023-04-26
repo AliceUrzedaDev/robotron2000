687a1f296a687e2466bacc4a88600a316efb9514
Alice Urzeda <aliceferreiradev@gmail.com>
2023-04-26 17:28:09 -0300

    inserindo botão para alterar a cor do Robotron

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-✨ O projeto foi feito junto ao curso do Alura!
-<h1 align="center">Bem vindos à criação do Robotron-2000</h1>
-
-## ✔️ Tecnologias utilizadas:
-
-- ``HTML``
-- ``CSS``
-- ``JavaScript``
-
-## 📁 Acesso ao projeto
-
-Você pode acessar o projeto feito por mim clicando [aqui](https://aliceurzedadev.github.io/robotron2000/).
+# robotron-2000
--- js/main.js
+++ js/main.js
@@ -58,3 +58,22 @@ function atualizaEstatisticas (peca) {
     })
 }
 
+const botoesCor = document.querySelectorAll('.btn-cor');
+const robo = document.querySelector('.robo');
+
+botoesCor.forEach((botao) => {
+    botao.addEventListener("click", () => {
+        console.log('aplicando a cor ' + botao.dataset.cor + ' no Robotron 2000!');
+        robo.src = `img/Robotron2000-${botao.dataset.cor}.png`;
+        botao.classList.add('btn-ativo');
+
+
+        botoesCor.forEach((btn) => {
+            if(btn.dataset.cor != botao.dataset.cor)
+            btn.classList.remove('btn-ativo');
+        });
+
+    });
+
+
+});
